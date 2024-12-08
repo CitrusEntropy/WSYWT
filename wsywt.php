db0391cdc7e11a7e1dbe4f37b5ebec9290ee133b
Phoveroth <ardadsaw@gmail.com>
2024-12-09 02:05:54 +0300

    claening

--- wsywt.php
+++ wsywt.php
@@ -1,27 +1,19 @@
 <!DOCTYPE html>
-<html>
-
-	<head>
-		<title>WSYWT</title>
-		<link rel='stylesheet' type='text/css' href='wsywt.css'>
-	</head>
-
-	<body>
-		
-		<h1>What should you watch today?</h1>
-		<p>Probably the social network</p>
-		<form action="/action_page.php">
-    	<label for="fname">age:</label>
-		<input type="text" id="age" name="age"><br><br>
-		<input type="submit" value="Submit">
-		</form>
-
-	</body>
-
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>WSYWT</title>
+</head>
+<body>
+	<h1>What should you watch today?</h1>
+	<p>Probably the social network</p>
+	<form action="/action_page.php">
+	<label for="fname">age:</label>
+	<input type="text" id="age" name="age"><br><br>
+	<input type="submit" value="Submit">
+</body>
 </html>
-
-
-
 
 
 ["Frozen", "Cars", "Hunger Games", "The Social Network", "Wolf of Wallstreet", "The Godfather"]
