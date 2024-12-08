901fd133fca94ef01cb0dd85526de61432d4e4b9
Phoveroth <ardadsaw@gmail.com>
2024-12-09 01:52:42 +0300

    PHP era.

--- wsywt.php
+++ wsywt.php
@@ -1,19 +1,29 @@
 <!DOCTYPE html>
 <html>
+
 	<head>
 		<title>WSYWT</title>
 		<link rel='stylesheet' type='text/css' href='wsywt.css'>
-		<style>
-
-		</style>
 	</head>
+
 	<body>
-		<h1 class="title">What should you watch today?</h1>
+		
+		<h1>What should you watch today?</h1>
 		<p>Probably the social network</p>
 		<form action="/action_page.php">
-    <label for="fname">age:</label>
+    	<label for="fname">age:</label>
 		<input type="text" id="age" name="age"><br><br>
 		<input type="submit" value="Submit">
-</form>
+		</form>
+
 	</body>
+
 </html>
+
+
+
+
+
+["Frozen", "Cars", "Hunger Games", "The Social Network", "Wolf of Wallstreet", "The Godfather"]
+
+["0-8", "9-16", "17-24", "25-32", "33-40", "40+"]
