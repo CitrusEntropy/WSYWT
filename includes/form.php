11ba995d7a6379c968c613dc9b246451176e1b84
Phoveroth <ardadsaw@gmail.com>
2024-12-09 02:46:05 +0300

    working prototype

---
+++ includes/form.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+        $movies = ["Frozen", "Cars", "Hunger Games", "The Social Network", "Wolf of Wallstreet", "The Godfather"];
+        $ages = ["0-7", "8-15", "16-23", "24-31", "32-39", "40+"];
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST" && $_POST["age"] >= 0)
+        {
+            $age = $_POST["age"];
+            if ($age/8 >= 5) $age = 5;
+            echo($movies[(int)($age/8)]);
+        } else
+        {
+            header("Location: ../index.php");
+        }
+
+    ?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>WSYWT</title>
+</head>
+<body>
+
+	<h1>What should you watch today?</h1>
+	<p>Probably the social network</p>
+
+	<form action="includes/form.php" method="post">
+		<label for="fname">age:</label>
+		<input type="number" id="age" name="age">
+		<br><br>
+		<input type="submit" value="Submit">
+	</form>
+
+</body>
+</html>
